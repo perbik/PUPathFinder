84a12efcd4e62f19fab3042a9e1e09d1a2fe2656
Fervicmar <fervicmardlagman@gmail.com>
2024-07-01 17:38:01 +0800

    Update facilities_list.dart

--- lib/model/facilities_list.dart
+++ lib/model/facilities_list.dart
@@ -6,6 +6,8 @@ List<FacilitiesModel> main_facilities_list = [
       "Some descriptions here blah blah"),
   FacilitiesModel("Building and Grounds Maintenance Office",
       "Some descriptions here blah blah"),
+  FacilitiesModel("Campus Development and Maintenance Building",
+      "Some descriptions here blah blah"), // verify if same as building and grounds maintenance office
   FacilitiesModel("Catwalk", "Some descriptions here blah blah"),
   FacilitiesModel("Centennial Monument", "Some descriptions here blah blah"),
   FacilitiesModel(
@@ -13,6 +15,7 @@ List<FacilitiesModel> main_facilities_list = [
   FacilitiesModel("East Gate", "Some descriptions here blah blah"),
   FacilitiesModel("Flagpole", "Some descriptions here blah blah"), // disable
   FacilitiesModel("Grandstand", "Some descriptions here blah blah"),
+  FacilitiesModel("Guard House", "Some descriptions here blah blah"),
   FacilitiesModel("Gymnasium", "Some descriptions here blah blah"),
   FacilitiesModel("Interfaith Chapel", "Some descriptions here blah blah"),
   FacilitiesModel("Laboratory High School", "Some descriptions here blah blah"),
@@ -30,11 +33,34 @@ List<FacilitiesModel> main_facilities_list = [
   FacilitiesModel(
       "Nemesio E. Prudente Freedom Park", "Some descriptions here blah blah"),
   FacilitiesModel("Ninoy Aquino Library", "Some descriptions here blah blah"),
+  FacilitiesModel(
+      "Non-Food Stalls Building", "Some descriptions here blah blah"),
+  FacilitiesModel("North Parking Area", "Some descriptions here blah blah"),
+  FacilitiesModel("Nutrition and Food Science Building",
+      "Some descriptions here blah blah"),
   FacilitiesModel("Open Court", "Some descriptions here blah blah"), // disable
   FacilitiesModel("Open University/Nontraditional Studies Program",
       "Some descriptions here blah blah"), // check if accurate yung facility
   FacilitiesModel("Oval Openfield", "Some descriptions here blah blah"),
+  FacilitiesModel(
+      "Physical Education Building", "Some descriptions here blah blah"),
+  FacilitiesModel(
+      "Printing Press Building", "Some descriptions here blah blah"),
+  FacilitiesModel(
+      "Property and Supply Building", "Some descriptions here blah blah"),
   FacilitiesModel("PUP Linear Park", "Some descriptions here blah blah"),
   FacilitiesModel("PUP Obelisk", "Some descriptions here blah blah"),
+  FacilitiesModel(
+      "PUP Sta. Mesa Ferry Station", "Some descriptions here blah blah"),
+  FacilitiesModel("Pylon", "Some descriptions here blah blah"),
+  FacilitiesModel(
+      "R.C. Overhead Water Tank", "Some descriptions here blah blah"),
+  FacilitiesModel(
+      "Souvenir Shop", "Some descriptions here blah blah"), // gazebo
+  FacilitiesModel("Student Canteen", "Some descriptions here blah blah"),
+  FacilitiesModel("Swimming Pool", "Some descriptions here blah blah"),
+  FacilitiesModel("Tahanan ng Alumni", "Some descriptions here blah blah"),
   FacilitiesModel("Tennis Court", "Some descriptions here blah blah"),
+  FacilitiesModel(
+      "Visitors Information Center", "Some descriptions here blah blah"),
 ];
